343dfbc4e64179e35eea6ee30197ceb418707728
fj0r <fj0rd@qq.com>
2025-12-16 18:19:38 +0800

    Add macro support for generating dispatch code with file tracking
    - Introduce `example` procedural macro
    - Add `attrs.rs` module for parsing macro input
    - Implement file change detection with `cargo:rerun-if-changed`

--- crates/macro/src/lib.rs
+++ crates/macro/src/lib.rs
@@ -1,1 +1,32 @@
+mod attrs;
 mod config;
+use attrs::Attrs;
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
+use quote::quote;
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use syn::{Error, parse_macro_input};
+
+macro_rules! bail {
+    ($($x: tt)*) => {
+        return Error::new(Span::call_site(), format!($($x)*))
+            .into_compile_error()
+            .into()
+    };
+}
+
+#[proc_macro]
+pub fn example(input: TokenStream) -> TokenStream {
+    let config = parse_macro_input!(input as Attrs)
+        .0
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+
+    let Some(file) = config.get("file") else {
+        bail!("must provide file");
+    };
+
+    println!("cargo:rerun-if-changed={}", file);
+    quote! {}.into()
+}
